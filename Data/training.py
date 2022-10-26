3986ea2e54514002430f90953aa794cce93e20dd
ldbaumga <42785786+ldbaumga@users.noreply.github.com>
2022-10-26 16:33:58 -0400

    converted pngs to jpgs

--- Data/training.py
+++ Data/training.py
@@ -1,40 +1,44 @@
 import os
-from tflite_model_maker.config import ExportFormat, QuantizationConfig
-from tflite_model_maker import model_spec
-from tflite_model_maker import object_detector
-
 import numpy as np
 
+from tflite_model_maker.config import QuantizationConfig
+from tflite_model_maker.config import ExportFormat
+from tflite_model_maker import model_spec
+from tflite_model_maker import object_detector
 
 from tflite_support import metadata
 
 import tensorflow as tf
 assert tf.__version__.startswith('2')
 
+
 tf.get_logger().setLevel('ERROR')
 from absl import logging
 logging.set_verbosity(logging.ERROR)
 
-lables = ['entity', 'weakentity', 'relationship', 'weakrelationship', 'attribute']
-
+classes = ['entity', 'weakentity', 'relationship', 'weakrelationship', 'attribute']
 train_data = object_detector.DataLoader.from_pascal_voc(
-    'TrainingData',
-    'TrainingData',
-    lables
+    'Data\TrainingData\Images',
+    'Data\TrainingData\Annotations',
+    label_map={1: 'entity', 2: 'weakentity', 3: 'relationship', 4: 'weakrelationship', 5: 'attribute'}
 )
 
 val_data = object_detector.DataLoader.from_pascal_voc(
-    'TestingData',
-    'TestingData',
-    lables
+    'Data\ValidationData\Images',
+    'Data\ValidationData\Annotations',
+    label_map={1: 'entity', 2: 'weakentity', 3: 'relationship', 4: 'weakrelationship', 5: 'attribute'}
 )
 
-spec = model_spec.get('efficientdet_lite0')
+test_data = object_detector.DataLoader.from_pascal_voc(
+    'Data\TestingData\Images',
+    'Data\TestingData\Annotations',
+    label_map={1: 'entity', 2: 'weakentity', 3: 'relationship', 4: 'weakrelationship', 5: 'attribute'}
+)
 
-model = object_detector.create(train_data, model_spec=spec, batch_size=4, train_whole_model=True, epochs=20, validation_data=val_data)
+spec = model_spec.get('efficientdet_lite4') # can use 0-4, 0 fast but inprecise
 
-model.evaluate(val_data)
+model = object_detector.create(train_data, model_spec=spec, batch_size=1, train_whole_model=True, epochs=20, validation_data=val_data)
 
-model.evaluate_tflite('android.tflite', val_data)
+model.evaluate(test_data)
 
-model.evaluate_tflite('android.tflite', val_data)
+model.export(export_dir='Data\Models')
