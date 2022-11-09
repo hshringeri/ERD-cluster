3fc6580db99387cb37950ba29adc09b90797eee2
yelshall <youssufshall@gmail.com>
2022-11-09 18:30:59 +0000

    Finish module2

--- Stage_1/__main__.py
+++ Stage_1/__main__.py
@@ -1,4 +1,5 @@
 # Created By: Youssuf Elshall, Lucas Baumgartner, Hardhi Shringeri, and Prajesh Sharma
+
 import module_1
 import module_2
 from colors import bcolors
@@ -7,10 +8,10 @@ from sys import platform
 
 def main():
     #User inputs a path to a file
-    MODULE = int(input(bcolors.OKCYAN+'Enter which module to run (1 or 2): '+bcolors.ENDC))
-    while MODULE != 1 and MODULE != 2:
+    MODULE = input(bcolors.OKCYAN+'Enter which module to run (1 or 2): '+bcolors.ENDC)
+    while MODULE != "1" and MODULE != "2":
         print(bcolors.WARNING+'Module input does not equal 1 or 2'+bcolors.ENDC)
-        MODULE = int(input(bcolors.OKCYAN+'Enter which module to run (1 or 2): '+bcolors.ENDC))
+        MODULE = input(bcolors.OKCYAN+'Enter which module to run (1 or 2): '+bcolors.ENDC)
 
     INPUT_IMAGE_PATH = input(bcolors.OKCYAN+'Enter a valid filepath to Img: '+bcolors.ENDC)
     #If it is invalid, loop until a vaid path is found
@@ -24,12 +25,12 @@ def main():
     TEMP_FILE = TEMP_FILE[0] + '-labled.png'
     TEMP_FILE_PATH[len(TEMP_FILE_PATH) - 1] = TEMP_FILE
     TEMP_FILE_PATH = '\\'.join(TEMP_FILE_PATH) if platform == "win32" else '/'.join(TEMP_FILE_PATH)
-    print(bcolors.OKGREEN+'Labeled image will be saved at: ' + TEMP_FILE_PATH + bcolors.ENDC)
 
-    if MODULE == 1:
+    if MODULE == "1":
+        print(bcolors.OKGREEN+'Labeled image will be saved at: ' + TEMP_FILE_PATH + bcolors.ENDC)
         boxes = module_1.run(INPUT_IMAGE_PATH, TEMP_FILE_PATH, True)
         print(boxes)
-    elif MODULE == 2:
+    elif MODULE == "2":
         boxes = module_1.run(INPUT_IMAGE_PATH, TEMP_FILE_PATH, False)
         text = module_2.get_all_text_from_image(INPUT_IMAGE_PATH, boxes)
         print(text)
--- Stage_1/module_2.py
+++ Stage_1/module_2.py
@@ -1,81 +1,115 @@
 # Created By: Youssuf Elshall, Lucas Baumgartner, Hardhi Shringeri, and Prajesh Sharma
+
 import cv2
 import pytesseract
-import os
 import numpy as np
+import re
+
 
 def open_image(img_path, coords):
     img = cv2.imread(img_path)
     crop = img[coords[2]:coords[4], coords[1]:coords[3]]
     return crop
 
-def zoom_at(img, zoom, coord=None):
-    """
-    Simple image zooming without boundary checking.
-    Centered at "coord", if given, else the image center.
 
-    img: numpy.ndarray of shape (h,w,:)
-    zoom: float
-    coord: (float, float)
-    """
-    # Translate to zoomed coordinates
-    h, w = [ zoom * i for i in img.shape ]
-    
-    if coord is None: cx, cy = w/2, h/2
-    else: cx, cy = [ zoom*c for c in coord ]
-    
-    img = cv2.resize( img, (0, 0), fx=zoom, fy=zoom)
-    img = img[ int(round(cy - h/zoom * .3)) : int(round(cy + h/zoom * .3)),
-               int(round(cx - w/zoom * .6)) : int(round(cx + w/zoom * .6))]
-    
+def zoom_at(img, zoom, coord=None):
+    h, w = [zoom * i for i in img.shape]
+
+    if coord is None:
+        cx, cy = w/2, h/2
+    else:
+        cx, cy = [zoom*c for c in coord]
+
+    img = cv2.resize(img, (0, 0), fx=zoom, fy=zoom)
+    img = img[int(round(cy - h/zoom * .3)): int(round(cy + h/zoom * .3)),
+              int(round(cx - w/zoom * .6)): int(round(cx + w/zoom * .6))]
+
     return img
+
 
 def preprocess_image(img, object_type, i):
     scale_percent = 200
     width = int(img.shape[1] * scale_percent / 100)
     height = int(img.shape[0] * scale_percent / 100)
     dim = (width, height)
-    
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
 
-    img = cv2.GaussianBlur(img, (3,3), 0)
-    img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
 
     if object_type == "attribute":
-        blur = cv2.GaussianBlur(img, (3,3), 10)
+        blur = cv2.GaussianBlur(img, (3, 3), 10)
         ellipse_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (200, 1))
-        remove_circle = cv2.morphologyEx(blur, cv2.MORPH_OPEN, ellipse_kernel, iterations=1)
-        cnts = cv2.findContours(remove_circle, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        remove_circle = cv2.morphologyEx(
+            blur, cv2.MORPH_OPEN, ellipse_kernel, iterations=1)
+        cnts = cv2.findContours(
+            remove_circle, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         cnts = cnts[0] if len(cnts) == 2 else cnts[1]
         for c in cnts:
-            cv2.drawContours(img, [c], -1, (255,255,255), 5)
+            cv2.drawContours(img, [c], -1, (255, 255, 255), 5)
     elif object_type == "weakrelationship":
-        img = zoom_at(img, 1.2)
+        img = cv2.threshold(
+            img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+        img = cv2.erode(img, None, iterations=1)
+        img = zoom_at(img, 1.6)
+    elif object_type == "relationship":
+        img = cv2.threshold(
+            img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+        img = cv2.erode(img, None, iterations=1)
+        img = zoom_at(img, 1.6)
     elif object_type == "entity" or object_type == "weakentity":
-        horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (25,1))
-        remove_horizontal = cv2.morphologyEx(img, cv2.MORPH_CLOSE, horizontal_kernel, iterations=3)
-        cnts = cv2.findContours(remove_horizontal, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        gray = cv2.bitwise_not(img)
+        bw = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C,
+                                   cv2.THRESH_BINARY, 15, -2)
+        horizontal = np.copy(bw)
+        vertical = np.copy(bw)
+
+        cols = horizontal.shape[1]
+        horizontal_size = cols / 15
+        horizontalStructure = cv2.getStructuringElement(
+            cv2.MORPH_RECT, (int(horizontal_size), 1))
+        horizontal = cv2.erode(horizontal, horizontalStructure)
+        horizontal = cv2.dilate(horizontal, horizontalStructure)
+
+        rows = vertical.shape[0]
+        verticalsize = rows / 15
+        verticalStructure = cv2.getStructuringElement(
+            cv2.MORPH_RECT, (1, int(verticalsize)))
+        vertical = cv2.erode(vertical, verticalStructure, iterations=3)
+        vertical = cv2.dilate(vertical, verticalStructure)
+
+        cnts = cv2.findContours(
+            horizontal, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-        mask = np.zeros(img.shape, np.uint8)
         for c in cnts:
-            cv2.drawContours(mask, [c], -1, (255,255,255),2)
-        img_dst = cv2.inpaint(img, mask, 3, cv2.INPAINT_TELEA)
+            cv2.drawContours(img, [c], -1, (255, 255, 255), 5)
 
+        cnts = cv2.findContours(
+            vertical, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+        for c in cnts:
+            cv2.drawContours(img, [c], -1, (255, 255, 255), 5)
 
-    cv2.imwrite("image" + str(i) +".png", img)
+    # cv2.imwrite("image" + str(i) +".png", img) uncomment to output imave
 
     return img
 
+
 def get_text_from_image(img):
-    custom_config = r'--oem 3 --psm 11'
+    custom_config = r'--oem 3 --psm 11 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_'
     return pytesseract.image_to_string(img, config=custom_config)
 
-# def clean_up_text(texts):
-#     cleaned_up = []
-#     for text in texts:
-        
-#     return cleaned_up
+
+def clean_up_text(texts):
+    cleaned_up = []
+
+    for text in texts:
+        text[1] = re.sub('\n', ' ', text[1])
+        text[1] = " ".join(text[1].split())
+        text[1] = re.sub(r'\b\w{1,2}\b', '', text[1])
+        cleaned_up.append([text[0], text[1].strip()])
+
+    return cleaned_up
+
 
 def get_all_text_from_image(img_path, all_coords):
     all_text = []
@@ -84,6 +118,4 @@ def get_all_text_from_image(img_path, all_coords):
         img = preprocess_image(img, coord[0], i)
         text = get_text_from_image(img)
         all_text.append([coord[0], text])
-    return all_text
-
-# print(get_all_text_from_image("../Collection1/045.png", [['attribute', 1741, 682, 2154, 843], ['relationship', 1663, 281, 2023, 559], ['weakrelationship', 2829, 1245, 3313, 1483], ['weakentity', 1869, 1964, 3024, 2946], ['weakrelationship', 1631, 1323, 2099, 1561], ['weakrelationship', 2, 1282, 485, 1530], ['weakentity', 232, 1862, 1317, 2591], ['entity', 2168, 262, 3187, 694], ['entity', 405, 264, 1433, 1165]]))
+    return clean_up_text(all_text)
